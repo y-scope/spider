0ca3944f3e532da78e79b08f79b889503bea2186
Sitao Wang <sitao.wang@mail.utoronto.ca>
2024-11-01 13:41:08 +0800

    build: Add script to build mariadb-connector-cpp from source

--- tools/scripts/linux/install-lib.sh
+++ tools/scripts/linux/install-lib.sh
@@ -11,5 +11,5 @@ lib_install_scripts_dir=$script_dir/..
 
 "$lib_install_scripts_dir"/lib_install/fmtlib.sh 11.0.2
 "$lib_install_scripts_dir"/lib_install/spdlog.sh 1.14.1
-"$lib_install_scripts_dir"/lib_install/mariadb-connector-c.sh 3.4.1
+"$lib_install_scripts_dir"/lib_install/mariadb-connector-cpp.sh 1.1.5
 "$lib_install_scripts_dir"/lib_install/boost.sh 1.86.0
