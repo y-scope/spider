b3c3620739ae36fd00d576a23e527c3ae689e521
Sitao Wang <sitao.wang@mail.utoronto.ca>
2024-11-05 07:52:07 +0800

    build: Reformat files and remove gcc-10 from linux build script

--- tools/scripts/lib_install/linux/install-dev.sh
+++ tools/scripts/lib_install/linux/install-dev.sh
@@ -8,29 +8,18 @@ set -u
 
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
-  ca-certificates \
-  checkinstall \
-  cmake \
-  curl \
-  g++ \
-  g++-10 \
-  gcc \
-  gcc-10 \
-  git \
-  libcurl4 \
-  libcurl4-openssl-dev \
-  libssl-dev \
-  make \
-  pkg-config \
-  python3 \
-  python3-pip \
-  python3-venv
-
-# Add alternatives for cpp-10, gcc-10, and g++-10
-# NOTE: We use a low priority to avoid affecting the prioritization of any existing alternatives
-# on the user's system.
-update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-10 0 \
-  --slave /usr/share/man/man1/cc.1.gz cc.1.gz /usr/share/man/man1/gcc.1.gz
-update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-10 0 \
-  --slave /usr/share/man/man1/c++.1.gz c++.1.gz /usr/share/man/man1/g++.1.gz
-update-alternatives --install /lib/cpp cpp /usr/bin/cpp-10 0
+    ca-certificates \
+    checkinstall \
+    cmake \
+    curl \
+    g++ \
+    gcc \
+    git \
+    libcurl4 \
+    libcurl4-openssl-dev \
+    libssl-dev \
+    make \
+    pkg-config \
+    python3 \
+    python3-pip \
+    python3-venv
