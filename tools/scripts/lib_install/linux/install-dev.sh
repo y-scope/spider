58efbbf233ad9bbc5af25b3b718089f8cc73b53e
Sitao Wang <sitao.wang@mail.utoronto.ca>
2024-11-04 23:57:12 +0800

    build: Reorder the library install scripts structure

---
+++ tools/scripts/lib_install/linux/install-dev.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Exit on any error
+set -e
+
+# Error on undefined variable
+set -u
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
+  ca-certificates \
+  checkinstall \
+  cmake \
+  curl \
+  g++ \
+  g++-10 \
+  gcc \
+  gcc-10 \
+  git \
+  libcurl4 \
+  libcurl4-openssl-dev \
+  libssl-dev \
+  make \
+  pkg-config \
+  python3 \
+  python3-pip
+
+# Add alternatives for cpp-10, gcc-10, and g++-10
+# NOTE: We use a low priority to avoid affecting the prioritization of any existing alternatives
+# on the user's system.
+update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-10 0 \
+  --slave /usr/share/man/man1/cc.1.gz cc.1.gz /usr/share/man/man1/gcc.1.gz
+update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-10 0 \
+  --slave /usr/share/man/man1/c++.1.gz c++.1.gz /usr/share/man/man1/g++.1.gz
+update-alternatives --install /lib/cpp cpp /usr/bin/cpp-10 0
--- tools/scripts/lib_install/linux/install-lib.sh
+++ tools/scripts/lib_install/linux/install-lib.sh
@@ -6,12 +6,10 @@ set -e
 # Error on undefined variable
 set -u
 
-brew update
-brew install \
-  boost \
-  fmt \
-  spdlog
-
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 lib_install_scripts_dir=$script_dir/..
-"${lib_install_scripts_dir}"/lib_install/mariadb-connector-cpp.sh 1.1.5
+
+"$lib_install_scripts_dir"/fmtlib.sh 11.0.2
+"$lib_install_scripts_dir"/spdlog.sh 1.14.1
+"$lib_install_scripts_dir"/mariadb-connector-cpp.sh 1.1.5
+"$lib_install_scripts_dir"/boost.sh 1.86.0
