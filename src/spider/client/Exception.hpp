064edd8fa5a6e6eb1005daaabfe9fcc36dd9a3d1
Sitao Wang <sitaowang1998@outlook.com>
2024-11-20 07:47:44 +0000

    Add exception to interface

---
+++ src/spider/client/Exception.hpp
@@ -0,0 +1,19 @@
+#ifndef SPIDER_CLIENT_EXCEPTION_HPP
+#define SPIDER_CLIENT_EXCEPTION_HPP
+
+#include <cstdint>
+#include <exception>
+
+namespace spider {
+
+enum class ExceptionCode: std::uint8_t {
+    ConnectionError,
+
+};
+
+struct SpiderException: std::exception {
+    ExceptionCode code;
+};
+
+}
+#endif // SPIDER_CLIENT_EXCEPTION_HPP
