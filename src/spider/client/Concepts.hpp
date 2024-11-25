7796e150b528f6a1a302a080309058c2708c2e98
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-11-25 08:30:24 -0500

    Refactor Concepts.hpp.

--- src/spider/client/Concepts.hpp
+++ src/spider/client/Concepts.hpp
@@ -7,19 +7,37 @@
 #include "Data.hpp"
 
 namespace spider {
-
-template <class, template <class...> class>
+// The template and partial specialization below check whether a given type is a specialization of
+// a given type.
+/**
+ * Template to check if a given type is specialization of a given template type.
+ *
+ * NOTE: This inherits from `std::false_type` so that by default, `Type` is not considered
+ * a specialization of `TemplateType`. The partial specialization of `IsSpecialization` below
+ * defines the case where `Type` is considered a specialization of `TemplateType`.
+ *
+ * @tparam Type
+ * @tparam TemplateType
+ */
+template <typename Type, template <typename...> class TemplateType>
 struct IsSpecialization : public std::false_type {};
 
-template <template <class...> class u, class... Ts>
-struct IsSpecialization<u<Ts...>, u> : public std::true_type {};
+// Specialization of `IsSpecialization` that inherits from `std::true_type` only when the first
+// template argument is a specialization (i.e., the same type with template parameters) of the
+// second template argument.
+template <template <typename...> class Type, class... TypeParams>
+struct IsSpecialization<Type<TypeParams...>, Type> : public std::true_type {};
 
-template <class T, template <class, class...> class u>
-inline constexpr bool cIsSpecializationV = IsSpecialization<T, u>::value;
+template <class Type, template <typename...> class TemplateType>
+inline constexpr bool cIsSpecializationV = IsSpecialization<Type, TemplateType>::value;
 
+/**
+ * Concept that represents the input to or output from a Task.
+ *
+ * @tparam T
+ */
 template <class T>
 concept TaskIo = Serializable<T> || cIsSpecializationV<T, Data>;
-
 }  // namespace spider
 
 #endif  // SPIDER_CLIENT_CONCEPTS_HPP
