80f314a84284a55f6e10978db01af07ac3d60b6b
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-11-25 08:16:55 -0500

    Refactor TaskGraph.hpp.

--- src/spider/client/TaskGraph.hpp
+++ src/spider/client/TaskGraph.hpp
@@ -1,5 +1,5 @@
-#ifndef SPIDER_CLIENT_TASKGRAPH_CPP
-#define SPIDER_CLIENT_TASKGRAPH_CPP
+#ifndef SPIDER_CLIENT_TASKGRAPH_HPP
+#define SPIDER_CLIENT_TASKGRAPH_HPP
 
 #include <functional>
 #include <memory>
@@ -7,33 +7,43 @@
 #include "Concepts.hpp"
 
 namespace spider {
-
-class TaskContext;
-
-template <TaskIo ReturnType, TaskIo... TaskParams>
-using TaskFunction = std::function<ReturnType(TaskContext, TaskParams...)>;
-
 class TaskGraphImpl;
 
 /**
- * TaskGraph represents a DAG of tasks.
+ * A TaskGraph represents a directed acyclic graph (DAG) of tasks.
  *
  * @tparam ReturnType
  * @tparam Params
  */
 template <TaskIo ReturnType, TaskIo... Params>
 class TaskGraph {
-public:
 private:
     std::unique_ptr<TaskGraphImpl> m_impl;
 };
 
-template <class T>
+// Forward declare `TaskContext` since `TaskFunction` takes `TaskContext` as a param, and
+// `TaskContext` uses `TaskFunction` as a param in its methods.
+class TaskContext;
+
+/**
+ * A function that can be run as a task on Spider.
+ *
+ * @tparam ReturnType
+ * @tparam TaskParams
+ */
+template <TaskIo ReturnType, TaskIo... TaskParams>
+using TaskFunction = std::function<ReturnType(TaskContext, TaskParams...)>;
+
+/**
+ * Concept for an object that's runnable on Spider.
+ *
+ * @tparam T
+ */
+template <typename T>
 concept Runnable = cIsSpecializationV<T, TaskFunction> || cIsSpecializationV<T, TaskGraph>;
 
-template <class T>
+template <typename T>
 concept RunnableOrTaskIo = Runnable<T> || TaskIo<T>;
-
 }  // namespace spider
 
-#endif  // SPIDER_CLIENT_TASKGRAPH_CPP
+#endif  // SPIDER_CLIENT_TASKGRAPH_HPP
