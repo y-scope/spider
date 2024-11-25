5d15b2255972e65ab3d72631c0caedbb117b69ee
Kirk Rodrigues <2454684+kirkrodrigues@users.noreply.github.com>
2024-11-25 08:10:52 -0500

    Refactor Data.hpp

--- src/spider/client/Data.hpp
+++ src/spider/client/Data.hpp
@@ -12,69 +12,70 @@ namespace spider {
 class DataImpl;
 
 /**
- * Data represents metadata of data stored on external storage. Data provides hint for Spider of
- * metadata information like locality of the data to improve scheduling decision.
+ * A representation of data stored on external storage. This class allows the user to define:
+ * - how the data should be cleaned up (garbage collected) once it is no longer referenced.
+ * - the locality of the data.
+ *
  * Example:
- *     spider::Data<std::string> disk_file_data = spider::Data<std::string>::Builder()
+ * @code{.cpp}
+ * auto disk_file_data = spider::Data<std::string>::Builder()
  *         .set_locality("node_address", true)
  *         .set_cleanup([](std::string cont& path) { std::remove(path); })
  *         .build("/path/of/file");
+ * @endcode
  *
- * Data is passed in as input so the tasks can get the value of the data.
- *
- * @tparam T type of the value.
+ * @tparam T Type of the value.
  */
 template <Serializable T>
 class Data {
 public:
     /**
-     * Gets the values stored in Data.
      * @return The stored value.
      */
     auto get() -> T;
 
     /**
-     * Sets locality list of the data.
+     * Sets the data's locality, indicated by the nodes that contain the data.
      *
      * @param nodes
-     * @param hard true if the locality list is a hard requirement, false otherwise. Hard locality
-     * requirement means that data can only be accessed from `nodes`.
+     * @param hard Whether the data is only accessible from the given nodes (i.e., the locality is a
+     * hard requirement).
      */
     void set_locality(std::vector<std::string> const& nodes, bool hard);
 
     class Builder {
     public:
         /**
-         * Sets locality list of the data to build.
+         * Sets the data's locality, indicated by the nodes that contain the data.
          *
          * @param nodes
-         * @param hard true if the locality list is a hard requirement, false otherwise
+         * @param hard Whether the data is only accessible from the given nodes (i.e., the locality
+         * is a hard requirement.
          * @return self
          */
-        auto set_locality(std::vector<std::string> const& nodes, bool hard) -> Data<T>::Builder&;
+        auto set_locality(std::vector<std::string> const& nodes, bool hard) -> Builder&;
 
         /**
-         * Defines clean up function of the data to build.
+         * Sets the cleanup function for the data. This function will be called when the data is no
+         * longer referenced.
          *
          * @param f
          * @return self
          */
-        auto set_cleanup(std::function<T const&()> const& f) -> Data<T>::Builder&;
+        auto set_cleanup(std::function<T const&()> const& f) -> Builder&;
 
         /**
-         * Builds the data. Stores the value of data into storage with locality list and cleanup
-         * functions.
+         * Builds the data object.
          *
          * @param t Value of the data
-         * @return Data object created.
+         * @return The built object.
          */
-        auto build(T const& t) -> Data<T>;
+        auto build(T const& t) -> Data;
     };
 
 private:
     std::unique_ptr<DataImpl> m_impl;
 };
-
 }  // namespace spider
 
 #endif  // SPIDER_CLIENT_DATA_HPP
