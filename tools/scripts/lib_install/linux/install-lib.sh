dbc7d7b4a2b992e6c6cb24eacdfbf7c27a062cbf
sitaowang1998 <sitaowang1998@outlook.com>
2024-11-29 11:42:57 -0500

    Update build scripts
    Bump spdlog version to solve conflict with fmtlib. Add using external
    fmt to spdlog build. Add PIC in fmt build. Add process to boost build.

--- tools/scripts/lib_install/linux/install-lib.sh
+++ tools/scripts/lib_install/linux/install-lib.sh
@@ -21,7 +21,7 @@ script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 lib_install_scripts_dir=$script_dir/..
 
 "$lib_install_scripts_dir"/fmtlib.sh 11.0.2
-"$lib_install_scripts_dir"/spdlog.sh 1.14.1
+"$lib_install_scripts_dir"/spdlog.sh 1.15.0
 "$lib_install_scripts_dir"/mariadb-connector-cpp.sh 1.1.5
 "$lib_install_scripts_dir"/boost.sh 1.86.0
 "$lib_install_scripts_dir"/msgpack.sh 7.0.0
