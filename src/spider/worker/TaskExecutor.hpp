3fc42367eb6093b5798e9ef898c09a003aede76d
sitaowang1998 <sitaowang1998@outlook.com>
2024-11-30 12:11:37 -0500

    Add task executor with tests and dll loader

---
+++ src/spider/worker/TaskExecutor.hpp
@@ -0,0 +1,112 @@
+#ifndef SPIDER_WORKER_TASKEXECUTOR_HPP
+#define SPIDER_WORKER_TASKEXECUTOR_HPP
+
+#include <cstdint>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+#include <absl/container/flat_hash_map.h>
+#include <boost/filesystem/path.hpp>
+#include <boost/process/v2/environment.hpp>
+#include <boost/process/v2/process.hpp>
+#include <boost/process/v2/stdio.hpp>
+
+#include "../core/BoostAsio.hpp"  // IWYU pragma: keep
+#include "../core/MsgPack.hpp"  // IWYU pragma: keep
+#include "FunctionManager.hpp"
+#include "message_pipe.hpp"
+
+namespace spider::worker {
+
+enum class TaskExecutorState : std::uint8_t {
+    Running,
+    Waiting,
+    Succeed,
+    Error,
+    Cancelled,
+};
+
+class TaskExecutor {
+public:
+    template <class... Args>
+    explicit TaskExecutor(
+            boost::asio::io_context& context,
+            std::string const& func_name,
+            std::vector<std::string> const& libs,
+            absl::flat_hash_map<
+                    boost::process::v2::environment::key,
+                    boost::process::v2::environment::value> const& environment,
+            Args&&... args
+    )
+            : m_read_pipe(context),
+              m_write_pipe(context) {
+        std::vector<std::string> process_args{"--func", func_name, "--libs"};
+        process_args.insert(process_args.end(), libs.begin(), libs.end());
+        boost::filesystem::path const exe = boost::process::v2::environment::find_executable(
+                "spider_task_executor",
+                environment
+        );
+        m_process = std::make_unique<boost::process::v2::process>(
+                context,
+                exe,
+                process_args,
+                boost::process::v2::process_stdio{
+                        .in = m_write_pipe,
+                        .out = m_read_pipe,
+                        .err = {/*stderr to default*/}
+                },
+                boost::process::v2::process_environment{environment}
+        );
+
+        // Set up handler for output file
+        boost::asio::co_spawn(context, process_output_handler(), boost::asio::detached);
+
+        // Send args
+        msgpack::sbuffer args_request = core::create_args_request(std::forward<Args>(args)...);
+        send_message(m_write_pipe, args_request);
+    }
+
+    TaskExecutor(TaskExecutor const&) = delete;
+    auto operator=(TaskExecutor const&) -> TaskExecutor& = delete;
+    TaskExecutor(TaskExecutor&&) = delete;
+    auto operator=(TaskExecutor&&) -> TaskExecutor& = delete;
+    ~TaskExecutor() = default;
+
+    auto completed() -> bool;
+    auto waiting() -> bool;
+    auto succeed() -> bool;
+    auto error() -> bool;
+
+    void wait();
+
+    void cancel();
+
+    template <class T>
+    auto get_result() -> std::optional<T> {
+        return core::response_get_result<T>(m_result_buffer);
+    }
+
+    [[nodiscard]] auto get_error() const -> std::tuple<core::FunctionInvokeError, std::string>;
+
+private:
+    auto process_output_handler() -> boost::asio::awaitable<void>;
+
+    std::mutex m_state_mutex;
+    TaskExecutorState m_state = TaskExecutorState::Running;
+
+    // Use `std::unique_ptr` to work around requirement of default constructor
+    std::unique_ptr<boost::process::v2::process> m_process = nullptr;
+    boost::asio::readable_pipe m_read_pipe;
+    boost::asio::writable_pipe m_write_pipe;
+
+    msgpack::sbuffer m_result_buffer;
+};
+
+}  // namespace spider::worker
+
+#endif  // SPIDER_WORKER_TASKEXECUTOR_HPP
---
+++ src/spider/worker/task_executor.cpp
@@ -0,0 +1,126 @@
+
+#include <unistd.h>
+
+#include <exception>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include <boost/any/bad_any_cast.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/value_semantic.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <fmt/format.h>
+#include <spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>  // IWYU pragma: keep
+
+#include "../core/BoostAsio.hpp"  // IWYU pragma: keep
+#include "../core/MsgPack.hpp"  // IWYU pragma: keep
+#include "DllLoader.hpp"
+#include "FunctionManager.hpp"
+#include "message_pipe.hpp"
+#include "TaskExecutorMessage.hpp"
+
+namespace {
+
+auto parse_arg(int const argc, char** const& argv) -> boost::program_options::variables_map {
+    boost::program_options::options_description desc;
+    desc.add_options()("help", "spider task executor")("func", boost::program_options::value<std::string>(), "function to run")(
+            "libs",
+            boost::program_options::value<std::vector<std::string>>(),
+            "dynamic libraries that include the spider tasks"
+    );
+
+    boost::program_options::variables_map variables;
+    boost::program_options::store(
+            // NOLINTNEXTLINE(misc-include-cleaner)
+            boost::program_options::parse_command_line(argc, argv, desc),
+            variables
+    );
+    boost::program_options::notify(variables);
+    return variables;
+}
+
+}  // namespace
+
+constexpr int cCmdArgParseErr = 1;
+constexpr int cDllErr = 2;
+constexpr int cFuncArgParseErr = 3;
+constexpr int cResultSendErr = 4;
+constexpr int cOtherErr = 5;
+
+auto main(int const argc, char** argv) -> int {
+    // Set up spdlog to write to stderr
+    // NOLINTNEXTLINE(misc-include-cleaner)
+    spdlog::set_default_logger(spdlog::stderr_color_mt("stderr"));
+
+    boost::program_options::variables_map const args = parse_arg(argc, argv);
+
+    std::string func_name;
+    try {
+        if (!args.contains("func")) {
+            return cCmdArgParseErr;
+        }
+        func_name = args["func"].as<std::string>();
+        if (!args.contains("libs")) {
+            return cCmdArgParseErr;
+        }
+        std::vector<std::string> const libs = args["libs"].as<std::vector<std::string>>();
+        spider::worker::DllLoader& dll_loader = spider::worker::DllLoader::get_instance();
+        for (std::string const& lib : libs) {
+            if (false == dll_loader.load_dll(lib)) {
+                return cDllErr;
+            }
+        }
+    } catch (boost::bad_any_cast& e) {
+        return cCmdArgParseErr;
+    }
+
+    try {
+        // Set up asio
+        boost::asio::io_context context;
+        boost::asio::posix::stream_descriptor in(context, dup(STDIN_FILENO));
+        boost::asio::posix::stream_descriptor out(context, dup(STDOUT_FILENO));
+
+        // Get args buffer from stdin
+        std::optional<msgpack::sbuffer> request_buffer_option = spider::worker::receive_message(in);
+        if (!request_buffer_option.has_value()) {
+            spdlog::error("Cannot read args buffer request");
+            return cFuncArgParseErr;
+        }
+        msgpack::sbuffer const& request_buffer = request_buffer_option.value();
+        if (spider::worker::TaskExecutorRequestType::Arguments
+            != spider::worker::get_request_type(request_buffer))
+        {
+            spdlog::error("Expect args request.");
+            return cFuncArgParseErr;
+        }
+        msgpack::object const args_object = spider::worker::get_message_body(request_buffer);
+        msgpack::sbuffer args_buffer;
+        msgpack::packer packer{args_buffer};
+        packer.pack(args_object);
+
+        // Run function
+        spider::core::Function const* function
+                = spider::core::FunctionManager::get_instance().get_function(func_name);
+        if (nullptr == function) {
+            spider::worker::send_message(
+                    out,
+                    spider::core::create_error_response(
+                            spider::core::FunctionInvokeError::FunctionExecutionError,
+                            fmt::format("Function {} not found.", func_name)
+                    )
+            );
+            return cResultSendErr;
+        }
+        msgpack::sbuffer const result_buffer = (*function)(args_buffer);
+
+        // Write result buffer to stdout
+        spider::worker::send_message(out, result_buffer);
+    } catch (std::exception& e) {
+        spdlog::error("Exception thrown: {}", e.what());
+        return cOtherErr;
+    }
+    return 0;
+}
