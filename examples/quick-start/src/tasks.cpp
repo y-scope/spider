49acc363506ce3df171473eda6b102bb031b761e
sitao <sitaowang1998@outlook.com>
2025-01-07 20:28:22 -0500

    Add lint for example codes and fix clang tidy

--- examples/quick-start/src/tasks.cpp
+++ examples/quick-start/src/tasks.cpp
@@ -3,9 +3,10 @@
 #include <spider/client/spider.hpp>
 
 // Task function implementation
-auto sum(spider::TaskContext& context, int x, int y) -> int {
+auto sum(spider::TaskContext& /*context*/, int x, int y) -> int {
     return x + y;
 }
 
 // Register the task with Spider
+// NOLINTNEXTLINE(cert-err58-cpp)
 SPIDER_REGISTER_TASK(sum);
