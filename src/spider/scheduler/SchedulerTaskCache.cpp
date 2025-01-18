63ddc0f8d59ea2dc45a3c892f11d880f8bef7be5
sitao <sitaowang1998@outlook.com>
2025-01-18 18:03:20 -0500

    Add scheduler task cache

--- src/spider/scheduler/SchedulerTaskCache.cpp
+++ src/spider/scheduler/SchedulerTaskCache.cpp
@@ -1,1 +1,85 @@
 #include "SchedulerTaskCache.hpp"
+
+#include <chrono>
+#include <functional>
+#include <optional>
+#include <string>
+#include <vector>
+
+#include <boost/uuid/uuid.hpp>
+
+#include "../core/Task.hpp"
+
+namespace spider::core {
+
+namespace {
+constexpr int cUpdateCount = 100;
+constexpr int cUpdateInterval = 5;  // 10 ms
+}  // namespace
+
+auto SchedulerTaskCache::get_ready_task(
+        boost::uuids::uuid const& worker_id,
+        std::string const& worker_addr
+) -> std::optional<boost::uuids::uuid> {
+    bool const updated = should_fetch_tasks();
+    if (updated) {
+        fetch_ready_tasks();
+    }
+
+    std::optional<Task> task = pop_next_task(worker_id, worker_addr);
+    if (task.has_value()) {
+        m_update_count++;
+        return task.value().get_id();
+    }
+    if (updated) {
+        m_update_count++;
+        return std::nullopt;
+    }
+    fetch_ready_tasks();
+    task = pop_next_task(worker_id, worker_addr);
+    m_update_count++;
+    if (task.has_value()) {
+        return task.value().get_id();
+    }
+
+    return std::nullopt;
+}
+
+auto SchedulerTaskCache::pop_next_task(
+        boost::uuids::uuid const& worker_id,
+        std::string const& worker_addr
+) -> std::optional<Task> {
+    std::vector<Task> tasks;
+    tasks.reserve(m_tasks.size());
+    for (auto const& task : m_tasks) {
+        tasks.push_back(task.second);
+    }
+    std::optional<boost::uuids::uuid> const task_id = std::invoke(
+            m_get_next_task_function,
+            std::ref(tasks),
+            std::cref(worker_id),
+            std::cref(worker_addr)
+    );
+    if (!task_id.has_value()) {
+        return std::nullopt;
+    }
+
+    Task task = m_tasks.at(task_id.value());
+    m_tasks.erase(task_id.value());
+    return task;
+}
+
+auto SchedulerTaskCache::should_fetch_tasks() -> bool {
+    std::chrono::steady_clock::time_point const now = std::chrono::steady_clock::now();
+    if (m_last_update + std::chrono::milliseconds(cUpdateInterval) < now) {
+        return true;
+    }
+    return m_update_count > cUpdateCount;
+}
+
+void SchedulerTaskCache::fetch_ready_tasks() {
+    m_update_count = 0;
+    m_last_update = std::chrono::steady_clock::now();
+}
+
+}  // namespace spider::core
--- src/spider/scheduler/SchedulerTaskCache.hpp
+++ src/spider/scheduler/SchedulerTaskCache.hpp
@@ -1,8 +1,18 @@
 #ifndef SPIDER_SCHEDULER_SCHEDULERTASKCACHE_HPP
 #define SPIDER_SCHEDULER_SCHEDULERTASKCACHE_HPP
 
+#include <chrono>
+#include <cstddef>
+#include <functional>
 #include <memory>
+#include <optional>
+#include <string>
+#include <vector>
 
+#include <absl/container/flat_hash_map.h>
+#include <boost/uuid/uuid.hpp>
+
+#include "../core/Task.hpp"
 #include "../storage/DataStorage.hpp"
 #include "../storage/MetadataStorage.hpp"
 
@@ -12,14 +22,45 @@ class SchedulerTaskCache {
 public:
     SchedulerTaskCache(
             std::shared_ptr<MetadataStorage> const& metadata_store,
-            std::shared_ptr<DataStorage> const& data_store
+            std::shared_ptr<DataStorage> const& data_store,
+            size_t const num_tasks,
+            std::function<std::optional<boost::uuids::uuid>(
+                    std::vector<Task>& tasks,
+                    boost::uuids::uuid const& worker_id,
+                    std::string const& worker_addr
+            )> const& get_next_task_function
     )
             : m_metadata_store{metadata_store},
-              m_data_store{data_store} {}
+              m_data_store{data_store},
+              m_num_tasks{num_tasks},
+              m_get_next_task_function{get_next_task_function} {}
+
+    auto get_ready_task(boost::uuids::uuid const& worker_id, std::string const& worker_addr)
+            -> std::optional<boost::uuids::uuid>;
 
 private:
+    auto should_fetch_tasks() -> bool;
+
+    void fetch_ready_tasks();
+
+    auto pop_next_task(boost::uuids::uuid const& worker_id, std::string const& worker_addr)
+            -> std::optional<Task>;
+
     std::shared_ptr<MetadataStorage> m_metadata_store;
     std::shared_ptr<DataStorage> m_data_store;
+
+    absl::flat_hash_map<boost::uuids::uuid, Task> m_tasks;
+    std::chrono::steady_clock::time_point m_last_update;
+    size_t m_update_count = 0;
+
+    size_t m_num_tasks;
+
+    std::function<std::optional<boost::uuids::uuid>(
+            std::vector<Task>& tasks,
+            boost::uuids::uuid const& worker_id,
+            std::string const& worker_addr
+    )>
+            m_get_next_task_function;
 };
 
 }  // namespace spider::core
