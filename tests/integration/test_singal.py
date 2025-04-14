f7c9fbb643abda02d2de19bfafbde949dffb692c
sitaowang1998 <sitaowang1998@outlook.com>
2025-04-14 13:17:51 -0400

    Remove no-exit tests

--- tests/integration/test_singal.py
+++ tests/integration/test_singal.py
@@ -22,7 +22,7 @@ from .client import (
 )
 
 
-def start_scheduler_worker_no_exit(storage_url: str, scheduler_port: int, lib: str):
+def start_scheduler_worker(storage_url: str, scheduler_port: int, lib: str):
     dir_path = Path(__file__).resolve().parent
     dir_path = dir_path / ".." / ".." / "src" / "spider"
     scheduler_cmds = [
@@ -43,7 +43,6 @@ def start_scheduler_worker_no_exit(storage_url: str, scheduler_port: int, lib: s
         storage_url,
         "--libs",
         lib,
-        "--no-exit",
     ]
     worker_process = subprocess.Popen(worker_cmds)
 
@@ -54,20 +53,8 @@ scheduler_port = 6103
 
 
 @pytest.fixture(scope="function")
-def scheduler_worker_no_exit(storage):
-    scheduler_process, worker_process = start_scheduler_worker_no_exit(
-        storage_url=storage_url, scheduler_port=scheduler_port, lib="tests/libworker_test.so"
-    )
-    # Wait for 5 second to make sure the scheduler and worker are started
-    time.sleep(5)
-    yield scheduler_process, worker_process
-    worker_process.kill()
-    scheduler_process.kill()
-
-
-@pytest.fixture(scope="function")
 def scheduler_worker_signal(storage):
-    scheduler_process, worker_process = start_scheduler_worker_no_exit(
+    scheduler_process, worker_process = start_scheduler_worker(
         storage_url=storage_url, scheduler_port=scheduler_port, lib="tests/libsignal_test.so"
     )
     # Wait for 5 second to make sure the scheduler and worker are started
@@ -78,41 +65,6 @@ def scheduler_worker_signal(storage):
 
 
 class TestWorkerNoExit:
-    # Test that worker stop receiving tasks and does not exit when receiving SIGTERM if no-exit is set
-    def test_no_exit(self, storage, scheduler_worker_no_exit):
-        _, worker_process = scheduler_worker_no_exit
-        # Send SIGTERM should not kill worker
-        os.kill(worker_process.pid, signal.SIGTERM)
-        time.sleep(1)
-        assert worker_process.poll() is None
-
-        # New task should not be executed
-        task = Task(
-            id=uuid.uuid4(),
-            function_name="sum_test",
-            inputs=[
-                TaskInput(type="int", value=msgpack.packb(1)),
-                TaskInput(type="int", value=msgpack.packb(2)),
-            ],
-            outputs=[TaskOutput(type="int")],
-        )
-        graph = TaskGraph(
-            id=uuid.uuid4(),
-            tasks={task.id: task},
-            dependencies=[],
-        )
-        job_id = uuid.uuid4()
-        submit_job(storage, job_id, graph)
-        time.sleep(1)
-        assert get_task_state(storage, task.id) == "ready"
-
-        # Send a second SIGTERM should not kill worker
-        os.kill(worker_process.pid, signal.SIGTERM)
-        time.sleep(1)
-        assert worker_process.poll() is None
-
-        # Cleanup job
-        remove_job(storage, job_id)
 
     # Test that worker propagates the SIGTERM signal to the task executor
     def test_signal(self, storage, scheduler_worker_signal):
