63d70be8492e77b69c0e6b33648ed2dddabd1e1b
sitaowang1998 <sitaowang1998@outlook.com>
2025-04-17 10:02:08 -0400

    Fix comment for StopFlag.
    Co-authored-by: davidlion <davidlion2@protonmail.com>

--- src/spider/scheduler/scheduler.cpp
+++ src/spider/scheduler/scheduler.cpp
@@ -268,8 +268,8 @@ auto main(int argc, char** argv) -> int {
         spdlog::error("Failed to join thread: {}", e.what());
     }
 
-    // If stop token is triggered, i.e. SIGTERM was caught, set exit value as if SIGTERM is not
-    // handled.
+    // If SIGTERM was caught and StopFlag is requested, set the exit value to corresponding to
+    // SIGTERM.
     if (spider::core::StopToken::is_stop_requested()) {
         return cSignalExitBase + SIGTERM;
     }
