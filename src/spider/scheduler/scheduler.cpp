a6de0719c2d5aeb298609e657d6dc72be7e8ebf6
sitaowang1998 <sitaowang1998@outlook.com>
2025-04-23 15:54:13 -0400

    Fix comment typo.
    Co-authored-by: davidlion <davidlion2@protonmail.com>

--- src/spider/scheduler/scheduler.cpp
+++ src/spider/scheduler/scheduler.cpp
@@ -268,8 +268,7 @@ auto main(int argc, char** argv) -> int {
         spdlog::error("Failed to join thread: {}", e.what());
     }
 
-    // If SIGTERM was caught and StopFlag is requested, set the exit value to corresponding to
-    // SIGTERM.
+    // If SIGTERM was caught and StopFlag is requested, set the exit value corresponding to SIGTERM.
     if (spider::core::StopFlag::is_stop_requested()) {
         return cSignalExitBase + SIGTERM;
     }
