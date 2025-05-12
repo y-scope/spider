e2cad8f84716991e35b1385d7c6264326eea404f
sitaowang1998 <sitaowang1998@outlook.com>
2025-05-12 16:11:05 -0400

    Revert test storage url change

--- tests/integration/client.py
+++ tests/integration/client.py
@@ -72,7 +72,7 @@ def is_head_task(task_id: uuid.UUID, dependencies: List[Tuple[uuid.UUID, uuid.UU
     return not any(dependency[1] == task_id for dependency in dependencies)
 
 
-g_storage_url = "jdbc:mariadb://localhost:3306/spider-storage?user=spider&password=password"
+g_storage_url = "jdbc:mariadb://localhost:3306/spider_test?user=root&password=password"
 
 
 @pytest.fixture(scope="session")
