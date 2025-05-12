2e2ddf8ee8840ca873ded9b95760e661f2ef99ca
sitaowang1998 <sitaowang1998@outlook.com>
2025-05-12 12:17:07 -0400

    Add ExecutorHandle class

---
+++ src/spider/worker/ExecutorHandle.cpp
@@ -0,0 +1,30 @@
+#include "ExecutorHandle.hpp"
+
+#include <mutex>
+
+namespace spider::worker {
+auto ExecutorHandle::get_task_id() const -> std::optional<boost::uuids::uuid> {
+    std::lock_guard const lock_guard{m_mutex};
+    if (nullptr != m_executor) {
+        return m_task_id;
+    }
+    return std::nullopt;
+}
+
+auto ExecutorHandle::get_executor() const -> TaskExecutor* {
+    std::lock_guard const lock_guard{m_mutex};
+    return m_executor;
+}
+
+auto ExecutorHandle::set(boost::uuids::uuid const task_id, TaskExecutor* executor) -> void {
+    std::lock_guard const lock_guard{m_mutex};
+    m_task_id = task_id;
+    m_executor = executor;
+}
+
+auto ExecutorHandle::clear() -> void {
+    std::lock_guard const lock_guard{m_mutex};
+    m_task_id = boost::uuids::uuid{};
+    m_executor = nullptr;
+}
+}  // namespace spider::worker
---
+++ src/spider/worker/ExecutorHandle.hpp
@@ -0,0 +1,30 @@
+#ifndef SPIDER_WORKER_EXECUTORHANDLE_HPP
+#define SPIDER_WORKER_EXECUTORHANDLE_HPP
+
+#include <mutex>
+
+#include <boost/uuid/uuid.hpp>
+
+#include "TaskExecutor.hpp"
+
+namespace spider::worker {
+/**
+ * Provides a thread-safe access to the task executor and other task related variables across
+ * threads.
+ */
+class ExecutorHandle {
+public:
+    [[nodiscard]] auto get_task_id() const -> std::optional<boost::uuids::uuid>;
+    [[nodiscard]] auto get_executor() const -> TaskExecutor*;
+    auto set(boost::uuids::uuid task_id, TaskExecutor* executor) -> void;
+    auto clear() -> void;
+
+private:
+    boost::uuids::uuid m_task_id;  // The task id is only valid if there is an executor.
+    TaskExecutor* m_executor
+            = nullptr;  // Do not use std::shared_ptr to avoid calling destructor twice.
+    std::mutex m_mutex;
+};
+}  // namespace spider::worker
+
+#endif
