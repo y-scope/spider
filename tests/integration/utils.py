6936a9b448a6ce4ececbc97d53e23b67569850e4
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-04 19:39:24 -0400

    Fix socket name type

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -2,12 +2,16 @@
 
 import socket
 
+IPv4Addr = tuple[str, int]
+IPv6Addr = tuple[str, int, int, int]
+AddrType = IPv4Addr | IPv6Addr
 
 def _get_free_tcp_port() -> int:
     """Returns a free TCP port."""
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind(("127.0.0.1", 0))
-        return s.getsockname()[1]
+        addr: AddrType = s.getsockname()
+        return addr[1]
 
 
 g_scheduler_port = _get_free_tcp_port()
