68992fcd4daac3ca8e93b897aa8b8fa8f921e469
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-05 15:00:38 -0400

    Fix ruff lint

---
+++ python/src/core/task.py
@@ -0,0 +1,5 @@
+"""Task module for Spider."""
+
+
+class Task:
+    """Represents a task in Spider."""
--- python/src/task_executor/task_executor.py
+++ python/src/task_executor/task_executor.py
@@ -1,3 +1,6 @@
+"""Executes a Spider Python task."""
+
+
 def main() -> None:
     """Main function to execute the task."""
 
