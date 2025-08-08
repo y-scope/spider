97af71bb7d0e0e86a7a34ec2a3dd7b45a1cb9898
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-08 13:33:10 -0400

    Add to_tdl_type_str to type package export

--- python/src/spider/type/__init__.py
+++ python/src/spider/type/__init__.py
@@ -1,5 +1,6 @@
 """Spider type package."""
 
+from spider.type.tdl_convert import to_tdl_type_str
 from spider.type.type import Double, Float, Int8, Int16, Int32, Int64
 
-__all__ = ["Double", "Float", "Int8", "Int16", "Int32", "Int64"]
+__all__ = ["Double", "Float", "Int8", "Int16", "Int32", "Int64", "to_tdl_type_str"]
