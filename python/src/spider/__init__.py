a7aa6b096bbf69ac99d97d331ca65ea4bca4ae1f
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-07 21:02:07 -0400

    Re-export spider.type under spider

--- python/src/spider/__init__.py
+++ python/src/spider/__init__.py
@@ -1,1 +1,12 @@
 """Spider package root."""
+
+from spider.type import Double, Float, Int8, Int16, Int32, Int64
+
+__all__ = [
+    "Double",
+    "Float",
+    "Int8",
+    "Int16",
+    "Int32",
+    "Int64",
+]
