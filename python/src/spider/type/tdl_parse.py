0a377f5bbfd1ee07e7830a81fd73c84dffd04339
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-09 17:43:15 -0400

    Add tests and fix parser

--- python/src/spider/type/tdl_parse.py
+++ python/src/spider/type/tdl_parse.py
@@ -2,29 +2,30 @@
 
 from copy import copy
 
-from lark import Token, Transformer, Tree, v_args
+from lark import Lark, Token, Transformer, v_args, Tree
 
 from spider.type.tdl_type import (
     BoolType,
     ClassType,
     DoubleType,
+    FloatType,
     Int8Type,
     Int16Type,
     Int32Type,
     Int64Type,
     ListType,
     MapType,
-    TdlType,
+    TdlType
 )
 
 grammar = r"""
 type: map_type | list_type | base_type
 
-map_type: "Map<" type_type "," type_type ">"
-list_type: "List<" type_type ">"
+map_type: "Map" "<" type "," type ">"
+list_type: "List" "<" type ">"
 base_type: ID
 
-ID: /[A-Za-z_][A-Za-z0-9_]*/
+ID: /[a-zA-Z_][a-zA-Z0-9_]*(\.[a-zA-Z_][a-zA-Z0-9_]*)*/
 
 %import common.WS
 %ignore WS
@@ -33,6 +34,7 @@ ID: /[A-Za-z_][A-Za-z0-9_]*/
 primitive_type_map = {
     "bool": BoolType(),
     "double": DoubleType(),
+    "float": FloatType(),
     "int8": Int8Type(),
     "int16": Int16Type(),
     "int32": Int32Type(),
@@ -40,18 +42,18 @@ primitive_type_map = {
 }
 
 
-class TypeTransformer(Transformer[Tree[Token], TdlType]):
-    """Transform Lark tree into TDL type."""
+class TypeTransformer(Transformer[Token, TdlType]):
+    """Transform Lark parse tree into TDL type."""
 
     @v_args(inline=True)
-    def map_type(self, key: TdlType, value: TdlType) -> TdlType:
+    def map_type(self, key: Tree, value: Tree) -> TdlType:
         """Transforms map node into Map type."""
-        return MapType(key, value)
+        return MapType(key.children[0], value.children[0])
 
     @v_args(inline=True)
-    def list_type(self, key: TdlType) -> TdlType:
+    def list_type(self, key: Tree) -> TdlType:
         """Transforms list node into Map type."""
-        return ListType(key)
+        return ListType(key.children[0])
 
     def base_type(self, children: list[Token]) -> TdlType:
         """Transforms primitive node into primitive type."""
@@ -59,3 +61,18 @@ class TypeTransformer(Transformer[Tree[Token], TdlType]):
         if name in primitive_type_map:
             return copy(primitive_type_map[name])
         return ClassType(name)
+
+parser = Lark(grammar, start="type", parser="lalr")
+
+def parse_tdl_type(string: str) -> TdlType:
+    """
+    Parses TDL type string into TDL type.
+    :param string: TDL type string.
+    :return: Parsed TDL type.
+    :raise: TypeError if string is not a valid TDL type.
+    """
+    tree = parser.parse(string)
+    try:
+        return TypeTransformer(visit_tokens=False).transform(tree).children[0]
+    except IndexError:
+        raise TypeError(f"'{string}' is not a valid TDL type.")
--- python/src/spider/type/tdl_type.py
+++ python/src/spider/type/tdl_type.py
@@ -1,8 +1,12 @@
 """Spider TDL types."""
-
+import importlib
 from abc import ABC, abstractmethod
+from types import GenericAlias
+from typing import cast
 
 from typing_extensions import override
+
+from spider.type.type import Double, Float, Int8, Int16, Int32, Int64
 
 
 class TdlType(ABC):
@@ -12,6 +16,10 @@ class TdlType(ABC):
     def type_str(self) -> str:
         """:return: String representation of the TDL type."""
 
+    @abstractmethod
+    def native_type(self) -> type | GenericAlias:
+        """:return: Native Python type of the TDL type."""
+
 
 class DoubleType(TdlType):
     """TDL double type."""
@@ -20,6 +28,9 @@ class DoubleType(TdlType):
     def type_str(self) -> str:
         return "double"
 
+    @override
+    def native_type(self) -> type | GenericAlias:
+        return Double
 
 class FloatType(TdlType):
     """TDL float type."""
@@ -28,6 +39,9 @@ class FloatType(TdlType):
     def type_str(self) -> str:
         return "float"
 
+    @override
+    def native_type(self) -> type | GenericAlias:
+        return Float
 
 class Int8Type(TdlType):
     """TDL int8 type."""
@@ -35,6 +49,10 @@ class Int8Type(TdlType):
     @override
     def type_str(self) -> str:
         return "int8"
+
+    @override
+    def native_type(self) -> type | GenericAlias:
+        return Int8
 
 
 class Int16Type(TdlType):
@@ -44,6 +62,9 @@ class Int16Type(TdlType):
     def type_str(self) -> str:
         return "int16"
 
+    @override
+    def native_type(self) -> type | GenericAlias:
+        return Int16
 
 class Int32Type(TdlType):
     """TDL int32 type."""
@@ -52,6 +73,9 @@ class Int32Type(TdlType):
     def type_str(self) -> str:
         return "int32"
 
+    @override
+    def native_type(self) -> type | GenericAlias:
+        return Int32
 
 class Int64Type(TdlType):
     """TDL int64 type."""
@@ -60,6 +84,9 @@ class Int64Type(TdlType):
     def type_str(self) -> str:
         return "int64"
 
+    @override
+    def native_type(self) -> type | GenericAlias:
+        return Int64
 
 class BoolType(TdlType):
     """TDL bool type."""
@@ -68,6 +95,33 @@ class BoolType(TdlType):
     def type_str(self) -> str:
         return "bool"
 
+    @override
+    def native_type(self) -> type | GenericAlias:
+        return bool
+
+def get_class_name(cls: type) -> str:
+    """
+    Gets Full class name.
+    :param cls:
+    :return:
+    """
+    return f"{cls.__module__}.{cls.__qualname__}"
+
+def get_class_by_name(name: str) -> type:
+    """
+    Gets class by name.
+    :param name:
+    :return:
+    :raise: TypeError if `class_name` is not a valid class.
+    """
+    parts = name.split(".")
+    module_name = ".".join(parts[:-1])
+    class_name = parts[-1]
+    try:
+        module = importlib.import_module(module_name)
+        return cast("type", getattr(module, class_name))
+    except Exception:
+        raise TypeError(f"'{name}' is not a valid class.")
 
 class ClassType(TdlType):
     """TDL Custom class type."""
@@ -83,6 +137,13 @@ class ClassType(TdlType):
     def type_str(self) -> str:
         return self.name
 
+    @override
+    def native_type(self) -> type | GenericAlias:
+        """
+        :return: Native Python type of the class.
+        :raise: TypeError if `class_name` is not a valid class.
+        """
+        return get_class_by_name(self.name)
 
 class ListType(TdlType):
     """TDL List type."""
@@ -95,6 +156,9 @@ class ListType(TdlType):
     def type_str(self) -> str:
         return f"List<{self.key.type_str()}>"
 
+    @override
+    def native_type(self) -> type | GenericAlias:
+        return list[self.key.native_type()]
 
 def is_integral(tdl_type: TdlType) -> bool:
     """:return: If TDL type is an integral type."""
@@ -128,3 +192,7 @@ class MapType(TdlType):
     @override
     def type_str(self) -> str:
         return f"Map<{self.key.type_str()},{self.value.type_str()}>"
+
+    @override
+    def native_type(self) -> type | GenericAlias:
+        return dict[self.key.native_type(), self.value.native_type()]
