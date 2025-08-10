f3ac001ea9c5c89fe60505c30e707dd8e60f82af
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-10 16:08:18 -0400

    Fix task IO type and add create_task from function

--- python/src/spider/client/task.py
+++ python/src/spider/client/task.py
@@ -1,6 +1,13 @@
 """Spider client task module."""
 
-from typing import ParamSpec, Protocol, TypeVar
+import inspect
+from types import FunctionType
+from typing import Protocol, runtime_checkable
+
+from spider import core
+from spider.client.data import Data
+from spider.core import TaskInput, TaskOutput, TaskOutputValue
+from spider.type import to_tdl_type_str
 
 
 class TaskContext:
@@ -9,13 +16,54 @@ class TaskContext:
     # TODO: Implement task context for use in task executor
 
 
-P = ParamSpec("P")
-R_co = TypeVar("R_co", covariant=True)
+@runtime_checkable
+class TaskFunction(Protocol):
+    """Task function must accept a TaskContext as its first argument."""
 
-
-class TaskFunction(Protocol[P, R_co]):
-    """Task function accepts TaskContext as its first argument."""
-
-    def __call__(self, context: TaskContext, *args: P.args, **kwargs: P.kwargs) -> R_co:
-        """Task function accepts TaskContext as its first argument."""
+    def __call__(self, context: TaskContext, *args: object) -> object:
+        """Task function must accept TaskContext as its first argument."""
         ...
+
+
+def create_task(func: TaskFunction) -> core.Task:
+    """
+    Creates a core Task object from the task function.
+    :param func:
+    :return:
+    :raise TypeError: If the function signature contains unsupported types.
+    """
+    task = core.Task()
+    if not isinstance(func, FunctionType):
+        msg = "`func` is not a function."
+        raise TypeError(msg)
+    task.function_name = func.__qualname__
+    signature = inspect.signature(func)
+    params = list(signature.parameters.values())
+    if params[0].annotation is not TaskContext:
+        msg = "First argument is not a TaskContext."
+        raise TypeError(msg)
+    for param in params[1:]:
+        if param.annotation == inspect.Parameter.empty:
+            msg = "Argument must has type annotation"
+            raise TypeError(msg)
+        tdl_type_str = to_tdl_type_str(param.annotation)
+        task.task_inputs.append(TaskInput(tdl_type_str, None))
+    returns = signature.return_annotation
+    if returns == inspect.Parameter.empty:
+        msg = "Return type must has type annotation"
+        raise TypeError(msg)
+    if type(returns) is tuple:
+        for r in returns:
+            tdl_type_str = to_tdl_type_str(r)
+            if r is Data:
+                task.task_outputs.append(TaskOutput(tdl_type_str, TaskOutputValue()))
+            else:
+                task.task_outputs.append(TaskOutput(tdl_type_str, TaskOutputValue()))
+    else:
+        tdl_type_str = to_tdl_type_str(returns)
+        if returns is Data:
+            task.task_outputs.append(TaskOutput(tdl_type_str, TaskOutputValue()))
+        else:
+            task.task_outputs.append(TaskOutput(tdl_type_str, TaskOutputValue()))
+
+    return task
--- python/src/spider/client/taskgraph.py
+++ python/src/spider/client/taskgraph.py
@@ -1,6 +1,7 @@
 """Spider client TaskGraph module."""
 
 from spider import core
+from spider.client.task import create_task, TaskFunction
 
 
 class TaskGraph:
@@ -12,3 +13,18 @@ class TaskGraph:
     def __init__(self) -> None:
         """Initialize TaskGraph."""
         self._impl = core.TaskGraph()
+
+
+def group(tasks: list[TaskFunction | TaskGraph]) -> TaskGraph:
+    """
+    Groups task functions and task graph into a single task graph.
+    :param tasks: List of task functions or task graphs.
+    :return: The new task graph.
+    """
+    graph = TaskGraph()
+    for task in tasks:
+        if isinstance(task, TaskFunction):
+            graph._impl.add_task(create_task(task))
+        # TODO: Add task graph
+
+    return graph
