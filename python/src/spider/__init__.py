ec6802d5c8d8cd2254b32c9d9ef2d3b11cd1b64d
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-10 23:42:43 -0400

    Export TaskContext

--- python/src/spider/__init__.py
+++ python/src/spider/__init__.py
@@ -1,6 +1,6 @@
 """Spider package root."""
 
-from spider.client import chain, group, TaskGraph
+from spider.client import chain, group, TaskContext, TaskGraph
 from spider.type import Double, Float, Int8, Int16, Int32, Int64
 
 __all__ = [
@@ -10,6 +10,7 @@ __all__ = [
     "Int16",
     "Int32",
     "Int64",
+    "TaskContext",
     "TaskGraph",
     "chain",
     "group",
--- python/src/spider/client/__init__.py
+++ python/src/spider/client/__init__.py
@@ -1,8 +1,10 @@
 """Spider python client."""
 
+from .task import TaskContext
 from .taskgraph import chain, group, TaskGraph
 
 __all__ = [
+    "TaskContext",
     "TaskGraph",
     "chain",
     "group",
