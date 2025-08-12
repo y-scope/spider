44067abf79564a2a223273f4c4be7cf1cf6c9549
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-11 22:52:39 -0400

    Add jdbc url

---
+++ python/src/spider/storage/jdbc_url.py
@@ -0,0 +1,64 @@
+"""JDBC URL module."""
+
+import re
+from dataclasses import dataclass
+
+
+@dataclass
+class JdbcParameters:
+    """JDBC url parameters."""
+
+    protocol: str
+    host: str
+    database: str
+    port: int | None = None
+    user: str | None = None
+    password: str | None = None
+
+
+pattern = re.compile(
+    r"^(?P<protocol>[a-zA-Z][a-zA-Z0-9+.-]*(::[a-zA-Z0-9+.-]*)?)://"
+    r"(?P<host>[^:/?]+)"
+    r"(?::(?P<port>\d+))?"
+    r"/(?P<database>[^?]+)"
+    r"(?:\?(?P<query>[^#]+))?"
+)
+
+
+def parse_jdbc_url(url: str) -> JdbcParameters:
+    """
+    Parses a JDBC URL.
+    :param url:
+    :return:
+    :raises ValueError: If the JDBC URL is invalid.
+    """
+    match = pattern.match(url)
+    if not match:
+        msg = f"Invalid JDBC URL: {url}"
+        raise ValueError(msg)
+
+    groups = match.groupdict()
+    query = groups.get("query") or ""
+    query_params = dict(param.split("=", 1) for param in query.split("&") if "=" in param)
+
+    protocol = groups.get("protocol")
+    if not protocol:
+        msg = f"Protocol is required in JDBC URL: {url}"
+        raise ValueError(msg)
+    host = groups.get("host")
+    if not host:
+        msg = f"Host is required in JDBC URL: {url}"
+        raise ValueError(msg)
+    database = groups.get("database")
+    if not database:
+        msg = f"Database is required in JDBC URL: {url}"
+        raise ValueError(msg)
+
+    return JdbcParameters(
+        protocol=protocol,
+        host=host,
+        port=int(groups["port"]) if groups.get("port") else None,
+        database=database,
+        user=query_params.get("user"),
+        password=query_params.get("password"),
+    )
