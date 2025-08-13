e50e892fff127146d79b2958824c98408eea6b20
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-12 22:28:50 -0400

    Export driver

--- python/src/spider/__init__.py
+++ python/src/spider/__init__.py
@@ -1,10 +1,12 @@
 """Spider package root."""
 
-from spider.client import chain, group, TaskContext, TaskGraph
+from spider.client import chain, Data, Driver, group, TaskContext, TaskGraph
 from spider.type import Double, Float, Int8, Int16, Int32, Int64
 
 __all__ = [
+    "Data",
     "Double",
+    "Driver",
     "Float",
     "Int8",
     "Int16",
