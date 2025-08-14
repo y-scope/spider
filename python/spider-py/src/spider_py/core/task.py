ea764640373ee284f491bee2a36e4257595dbefb
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-13 21:46:04 -0400

    Fix merge

--- python/spider-py/src/spider_py/core/task.py
+++ python/spider-py/src/spider_py/core/task.py
@@ -1,5 +1,50 @@
 """Task module for Spider."""
 
+from dataclasses import dataclass, field
+from enum import IntEnum
+from uuid import UUID, uuid4
 
+from spider_py.core.data import DataId
+
+TaskId = UUID
+
+
+@dataclass
+class TaskInputOutput:
+    """Represents a task input that points to output of another task"""
+
+    task_id: TaskId
+    position: int
+
+
+TaskInputValue = bytes
+TaskInputData = DataId
+TaskInput = TaskInputOutput | TaskInputValue | TaskInputData
+
+TaskOutputValue = bytes
+TaskOutputData = DataId
+TaskOutput = TaskOutputValue | TaskOutputData
+
+
+class TaskState(IntEnum):
+    """Represents state of a task"""
+
+    Pending = 0
+    Ready = 1
+    Running = 2
+    Succeeded = 3
+    Failed = 4
+    Cancelled = 5
+
+
+@dataclass
 class Task:
     """Represents a task in Spider."""
+
+    task_id: TaskId = field(default_factory=uuid4)
+    function_name: str = ""
+    state: TaskState = TaskState.Pending
+    timeout: float = 0
+    max_retries: int = 0
+    task_inputs: list[TaskInput] = field(default_factory=list)
+    task_outputs: list[TaskOutput] = field(default_factory=list)
---
+++ python/spider-py/src/spider_py/core/taskgraph.py
@@ -0,0 +1,56 @@
+"""TaskGraph module for Spider."""
+
+from spider_py.core.task import Task, TaskId
+
+
+class TaskGraph:
+    """Represents a task graph in Spider."""
+
+    def __init__(self) -> None:
+        """Initializes an empty task graph."""
+        self.tasks: dict[TaskId, Task] = {}
+        # Dependency list consists of a list of tuples of
+        #   - parent task id
+        #   - child task id
+        self.dependencies: list[tuple[TaskId, TaskId]] = []
+        self.input_tasks: set[TaskId] = set()
+        self.output_tasks: set[TaskId] = set()
+
+    def add_task(
+        self, task: Task, parents: list[TaskId] | None = None, children: list[TaskId] | None = None
+    ) -> None:
+        """
+        Adds a task to the graph.
+        :param task: The task to add.
+        :param parents: The parent ids of the task. Must be already in the task graph.
+        :param children: The children ids of the task. Must be already in the task graph.
+        """
+        self.tasks[task.task_id] = task
+        if parents:
+            for parent in parents:
+                self.dependencies.append((parent, task.task_id))
+                self.output_tasks.discard(parent)
+        else:
+            self.input_tasks.add(task.task_id)
+        if children:
+            for child in children:
+                self.dependencies.append((task.task_id, child))
+                self.input_tasks.discard(child)
+        else:
+            self.output_tasks.add(task.task_id)
+
+    def get_parents(self, task_id: TaskId) -> list[Task]:
+        """
+        Gets parent tasks of task.
+        :param task_id: ID of the task.
+        :return: List of parent tasks.
+        """
+        return [self.tasks[parent] for (parent, child) in self.dependencies if child == task_id]
+
+    def get_children(self, task_id: TaskId) -> list[Task]:
+        """
+        Gets child tasks of task.
+        :param task_id: ID of the task.
+        :return: List of children tasks.
+        """
+        return [self.tasks[child] for (parent, child) in self.dependencies if parent == task_id]
