8a383c515418a0519d1efe71e14dac9911a936de
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-18 10:27:43 -0400

    Add argument parsing

--- python/src/spider/task_executor/task_executor.py
+++ python/src/spider/task_executor/task_executor.py
@@ -1,8 +1,24 @@
 """Executes a Spider Python task."""
 
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    """
+    Parses task executor arguments.
+    :return: Parsed arguments.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("func", type=str, required=True)
+    parser.add_argument("libs", nargs="+", type=str, required=True)
+    parser.add_argument("task_id", type=str, required=True)
+    parser.add_argument("storage_url", type=str, required=True)
+    return parser.parse_args()
+
 
 def main() -> None:
     """Main function to execute the task."""
+    args = parse_args()
 
 
 if __name__ == "__main__":
