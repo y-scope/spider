168683b0a9f14fd5c3191bb566ea7a5a573232dd
sitaowang1998 <sitaowang1998@outlook.com>
2025-08-18 13:02:44 -0400

    Fix ruff

--- python/src/spider/task_executor/task_executor.py
+++ python/src/spider/task_executor/task_executor.py
@@ -2,9 +2,11 @@
 
 import argparse
 import logging
+from io import BufferedReader
+from os import fdopen
 from uuid import UUID
 
-from src.spider.storage import MariaDBStorage
+from spider.storage import MariaDBStorage
 
 # Set up logger
 logger = logging.getLogger(__name__)
@@ -25,6 +27,27 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+HeaderSize = 16
+
+
+def receive_message(pipe: BufferedReader) -> bytes:
+    """
+    Receives message from the pipe with a size header.
+    :param pipe: Pipe to receive message from.
+    :return: Received message body.
+    :raises IOError: If read from pipe fails.
+    :raises UnicodeDecodeError: If parsing header fails.
+    :raises EOFError: If the message body size does not match header size.
+    """
+    body_size_str = pipe.read(HeaderSize).decode()
+    body_size = int(body_size_str, base=10)
+    body = pipe.read(body_size)
+    if len(body) != body_size:
+        msg = "Received message body size does not match header size."
+        raise EOFError(msg)
+    return body
+
+
 def main() -> None:
     """
     Main function to execute the task.
@@ -37,11 +60,17 @@ def main() -> None:
     task_id = args.task_id
     task_id = UUID(task_id)
     storage_url = args.storage_url
+    input_pipe = args.input_pipe
+    output_pipe = args.output_pipe
 
-    logger.debug(f"Function to run: {func}")
+    logger.debug("Function to run: %s", func)
 
     # Sets up storage
     storage = MariaDBStorage(storage_url)
+
+    input_pipe = fdopen(input_pipe, "rb")
+    output_pipe = fdopen(output_pipe, "wb")
+    input_data = receive_message(input_pipe)
 
 
 if __name__ == "__main__":
