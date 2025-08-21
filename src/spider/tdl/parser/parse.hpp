0d7d11c5f967b2849b313aa9e58ba2434dc252e6
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2025-08-20 20:39:13 -0400

    Parser error handling included.

--- src/spider/tdl/parser/parse.hpp
+++ src/spider/tdl/parser/parse.hpp
@@ -3,15 +3,21 @@
 
 #include <istream>
 
+#include <boost/outcome/std_result.hpp>
+
+#include <spider/tdl/Error.hpp>
+
 namespace spider::tdl::parser {
 /**
  * Parses a TDL file as a translation unit from an input stream.
  * @param input The input stream containing the TDL content. The entire content will be considered
  * as a single translation unit.
- * @return Whether the parser returned on success.
- * NOTE: We will update this function to return an actual AST root once we add Antlr actions.
+ * @return A void result on success, or an error specified by an `Error` instance on failure.
+ * NOTE: We will update this function to return an actual AST root on success once we add Antlr
+ * actions.
  */
-auto parse_translation_unit_from_istream(std::istream& input) -> bool;
+[[nodiscard]] auto parse_translation_unit_from_istream(std::istream& input)
+        -> boost::outcome_v2::std_checked<void, Error>;
 }  // namespace spider::tdl::parser
 
 #endif  // SPIDER_TDL_PARSER_PARSE_HPP
--- tests/tdl/test-parser.cpp
+++ tests/tdl/test-parser.cpp
@@ -6,49 +6,67 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include <spider/tdl/parser/parse.hpp>
+#include <spider/tdl/parser/SourceLocation.hpp>
 
 namespace {
-constexpr std::string_view cTestInput1{
-        "namespace test1 {\n"
-        "    // Function with no parameters and no return type\n"
-        "    fn empty_func();\n"
-        "\n"
-        "    // Function with parameters and return type\n"
-        "    fn add(a: int32, b: int32) -> int64;\n"
-        "\n"
-        "    // Function that returns an empty tuple\n"
-        "    fn return_empty_tuple() -> Tuple<>;\n"
-        "}\n"
-        "\n"
-        "struct Input {\n"
-        "    field_0: int8,\n"
-        "    field_1: int16,\n"
-        "    field_2: int32,\n"
-        "    field_3: int64,\n"
-        "    field_4: float,\n"
-        "    field_5: double,\n"
-        "    field_6: bool,\n"
-        "    field_7: List<int8>,\n"
-        "    field_8: Map<List<int8>, double>,\n"
-        "    field_9: Tuple<int8, int16, int32>,\n"
-        "};\n"
-        "\n"
-        "struct Output {\n"
-        "    // Notice that the field doesn't end with a comma\n"
-        "    processed_input: Map<int64, Input>\n"
-        "};\n"
-        "\n"
-        "namespace test2 {\n"
-        "    fn process_input(input: Input, task_id: int64) -> Output;\n"
-        "    fn process_inputs(inputs: List<Input>, task_id: int64) -> Output;\n"
-        "}"
+using spider::tdl::parser::parse_translation_unit_from_istream;
+using spider::tdl::parser::SourceLocation;
+
+constexpr std::string_view cTestInput1{R"(
+namespace test1 {
+    // Function with no parameters and no return type
+    fn empty_func();
+
+    // Function with parameters and return type
+    fn add(a: int32, b: int32) -> int64;
+
+    // Function that returns an empty tuple
+    fn return_empty_tuple() -> Tuple<>;
+}
+
+struct Input {
+    field_0: int8,
+    field_1: int16,
+    field_2: int32,
+    field_3: int64,
+    field_4: float,
+    field_5: double,
+    field_6: bool,
+    field_7: List<int8>,
+    field_8: Map<List<int8>, double>,
+    field_9: Tuple<int8, int16, int32>,
 };
 
-TEST_CASE("Parser test basic", "[tdl][parser]") {
-    using namespace spider::tdl::parser;
-    std::istringstream input_stream{std::string{cTestInput1}};
-    REQUIRE(parse_translation_unit_from_istream(input_stream));
+struct Output {
+    // Notice that the field doesn't end with a comma
+    processed_input: Map<int64, Input>
+};
+
+namespace test2 {
+    fn process_input(input: Input, task_id: int64) -> Output;
+    fn process_inputs(inputs: List<Input>, task_id: int64) -> Output;
 }
+)"};
+
+TEST_CASE("Parser test basic", "[tdl][parser]") {
+    std::istringstream input_stream{std::string{cTestInput1}};
+    REQUIRE_FALSE(parse_translation_unit_from_istream(input_stream).has_error());
+}
+
+TEST_CASE("Parser errors", "[tdl][parser]") {
+    SECTION("Namespace without an identifier") {
+        constexpr std::string_view cEmptyNamespaceInput{"namespace { fn empty_func(); }"};
+        std::istringstream input_stream{std::string{cEmptyNamespaceInput}};
+        auto const parse_result{parse_translation_unit_from_istream(input_stream)};
+        REQUIRE(parse_result.has_error());
+        auto const& error{parse_result.error()};
+
+        constexpr std::string_view cExpectedErrorMessage{"Parser: missing ID at '{'"};
+        constexpr SourceLocation cExpectedErrorLocation{1, 10};
+        REQUIRE(error.get_message() == cExpectedErrorMessage);
+        REQUIRE(error.get_source_location() == cExpectedErrorLocation);
+    }
+}  // namespace
 }  // namespace
 
 // NOLINTEND(cert-err58-cpp,cppcoreguidelines-avoid-do-while,readability-function-cognitive-complexity,cppcoreguidelines-avoid-non-const-global-variables,cppcoreguidelines-avoid-c-arrays,modernize-avoid-c-arrays)
