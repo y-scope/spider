0fb2e8d0d2265a2cb5fbd3514bbafd2875bdbeb0
sitaowang1998 <sitaowang1998@outlook.com>
2025-09-06 14:54:35 -0400

    Improve style and docstring

--- python/spider-py/src/spider_py/utils/msgpack_serde.py
+++ python/spider-py/src/spider_py/utils/msgpack_serde.py
@@ -5,12 +5,11 @@ from types import GenericAlias
 from typing import cast, get_args, get_origin
 
 
-def msgpack_encoder(obj: object) -> list[object] | object:
+def msgpack_encoder(obj: object) -> object:
     """
-    Encodes an object into a list of its field values.
-    This function recursively encodes nested dataclasses, lists, and dictionaries.
-    :param obj: Dataclass instance to serialize.
-    :return: List of field values.
+    Transforms an object into a less structured form comprised of lists or dicts.
+    :param obj:
+    :return: A less structured representation of `obj`.
     """
     if is_dataclass(obj):
         return {field.name: msgpack_encoder(getattr(obj, field.name)) for field in fields(obj)}
@@ -21,37 +20,9 @@ def msgpack_encoder(obj: object) -> list[object] | object:
     return obj
 
 
-def _decode_class(cls: type, data: object) -> object:
-    """
-    Deserializes the input data as a `cls` instance.
-    This function only works for non-container classes (not lists or dicts).
-    :param cls: Class to deserialize into.
-    :param data: Serialized data.
-    :return: A new `cls` instance containing the deserialized data.
-    :raise: TypeError if `data` is not compatible with `cls`.
-    """
-    msg = f"Cannot create instance of {cls} with {data!r}."
-    if is_dataclass(cls):
-        if not isinstance(data, dict):
-            raise TypeError(msg)
-        args = {}
-        parameters = {field.name: field for field in fields(cls)}
-        for name, value in data.items():
-            if name not in parameters:
-                raise TypeError(msg)
-            expected_field_type = parameters[name].type
-            if not isinstance(expected_field_type, (type, GenericAlias)):
-                raise TypeError(msg)
-            args[name] = msgpack_decoder(expected_field_type, value)
-        return cls(**args)
-
-    return cls(data)
-
-
 def msgpack_decoder(cls: type | GenericAlias, data: object) -> object:
     """
     Decodes data into an instance of `cls`.
-    This function recursively decodes nested dataclasses, lists, and dictionaries.
     :param cls: Class to deserialize into.
     :param data: Data to decode.
     :return: Instance of `cls`.
@@ -80,3 +51,30 @@ def msgpack_decoder(cls: type | GenericAlias, data: object) -> object:
         }
 
     raise TypeError(msg)
+
+
+def _decode_class(cls: type, data: object) -> object:
+    """
+    Deserializes the input data as a `cls` instance.
+    :param cls: Class to deserialize into.
+    :param data: Serialized data.
+    :return: A new `cls` instance containing the deserialized data.
+    :raise: TypeError if `data` is not compatible with `cls`.
+    """
+    msg = f"Cannot create instance of {cls} with {data!r}."
+    if not is_dataclass(cls):
+        # Fall back to normal constructor if `cls` is not a dataclass.
+        return cls(data)
+
+    if not isinstance(data, dict):
+        raise TypeError(msg)
+    args = {}
+    expected_fields = {field.name: field for field in fields(cls)}
+    for name, value in data.items():
+        if name not in expected_fields:
+            raise TypeError(msg)
+        expected_field_type = expected_fields[name].type
+        if not isinstance(expected_field_type, (type, GenericAlias)):
+            raise TypeError(msg)
+        args[name] = msgpack_decoder(expected_field_type, value)
+    return cls(**args)
