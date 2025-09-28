eb3029c7d076b0f8aca89247e84c94d7fd264022
LinZhihao-723 <zh.lin@mail.utoronto.ca>
2025-09-28 16:50:57 -0400

    Add a case without error.

--- tests/tdl/test-pass-analysis-DetectUndefinedStruct.cpp
+++ tests/tdl/test-pass-analysis-DetectUndefinedStruct.cpp
@@ -34,6 +34,20 @@ struct class1 {
 };
 )"};
 
+constexpr std::string_view cTestCase2{R"(// Start of a TDL file. This is line#1.
+struct class0 {
+    use_1: Map<int8, List<Map<int16, int32>>>,
+};
+
+namespace ns1 {
+    fn func0(input: class0) -> class1;
+}
+
+struct class1 {
+    use_0: class0,
+};
+)"};
+
 TEST_CASE("DetectUndefinedStruct Case 1", "[tdl][pass][analytics][DetectUndefinedStruct]") {
     std::istringstream input_stream{std::string{cTestCase1}};
     auto const parse_result{parse_translation_unit_from_istream(input_stream)};
@@ -56,6 +70,17 @@ TEST_CASE("DetectUndefinedStruct Case 1", "[tdl][pass][analytics][DetectUndefine
             "Referencing to an undefined struct `class2` at (19:37)"
     };
     REQUIRE(cExpectedErrorMessage == error->to_string());
+}
+
+TEST_CASE("DetectUndefinedStruct Case 2", "[tdl][pass][analytics][DetectUndefinedStruct]") {
+    std::istringstream input_stream{std::string{cTestCase2}};
+    auto const parse_result{parse_translation_unit_from_istream(input_stream)};
+    REQUIRE_FALSE(parse_result.has_error());
+    auto const& translation_unit{parse_result.value()};
+
+    auto detect_undefined_struct_pass{DetectUndefinedStruct{translation_unit.get()}};
+    auto const run_result{detect_undefined_struct_pass.run()};
+    REQUIRE_FALSE(run_result.has_error());
 }
 }  // namespace
 
