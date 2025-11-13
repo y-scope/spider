9a721eee38f086aaed4d96bbf67b630b73f0504c
sitaowang1998 <sitaowang1998@outlook.com>
2025-11-13 17:57:02 -0500

    Fix comment

--- python/spider-py/src/spider_py/storage/mariadb_storage.py
+++ python/spider-py/src/spider_py/storage/mariadb_storage.py
@@ -639,8 +639,7 @@ class MariaDBStorage(Storage):
         if status_str not in _StrToJobStatusMap:
             msg = f"Unknown job status: {status_str}."
             raise StorageError(msg)
-        # Old version of mariadb (1.0.11) requires a fetchall after a fetchone to drain the result
-        # set even if it is already empty.
+        # Uses fetchall after a fetchone to drain the result set even if it is already empty.
         cursor.fetchall()
         return _StrToJobStatusMap[status_str]
 
