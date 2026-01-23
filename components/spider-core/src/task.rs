706c0b2cd3fc2db9c0f73e5976078e2f8423370a
LinZhihao-723 <pleiades3190@gmail.com>
2026-01-22 20:07:58 -0500

    Add task descriptor

--- components/spider-core/src/task.rs
+++ components/spider-core/src/task.rs
@@ -1,3 +1,23 @@
+mod type_descriptor;
+
+use thiserror::Error;
+pub use type_descriptor::*;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("`serde_json::Error`: {0}")]
+    SerdeJsonError(#[from] serde_json::Error),
+
+    #[error("`rmp_serde::encode::Error`: {0}")]
+    RmpSerdeEncodeError(#[from] rmp_serde::encode::Error),
+
+    #[error("`rmp_serde::decode::Error`: {0}")]
+    RmpSerdeDecodeError(#[from] rmp_serde::decode::Error),
+
+    #[error("Invalid struct name: {0}")]
+    InvalidStructName(String),
+}
+
 /// Represents a task in the Spider scheduling framework.
 pub struct Task {}
 
