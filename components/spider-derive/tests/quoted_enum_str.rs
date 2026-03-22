e44ecf1eb0a3d0cf8464b295ec6760249bab167e
sitao <sitao.wang@mail.utoronto.ca>
2026-03-21 23:38:56 -0400

    Fix derive test

--- components/spider-derive/tests/quoted_enum_str.rs
+++ components/spider-derive/tests/quoted_enum_str.rs
@@ -14,8 +14,16 @@ enum Single {
     Only(i32),
 }
 
+impl PartialEq for Single {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (Self::Only(val1), Self::Only(val2)) => val1 == val2,
+        }
+    }
+}
+
 #[test]
-fn quoted_enum_str() {
+fn test_quoted_enum_str() {
     assert_eq!(Color::quoted_enum_str(), "'Red', 'Green', 'Blue'");
     assert_eq!(Single::quoted_enum_str(), "'Only'");
 }
