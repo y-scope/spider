0683275d84d7c2e318feb3788f0223792ae90b8d
Lin Zhihao <59785146+LinZhihao-723@users.noreply.github.com>
2026-06-07 15:58:48 -0400

    build: Split the Ubuntu dev-dependency install script into common, huntsman, and wolf variants. (#336)
    Co-authored-by: sitaowang1998 <sitaowang1998@outlook.com>

--- tools/scripts/lib_install/ubuntu/install-dev-wolf.sh
+++ tools/scripts/lib_install/ubuntu/install-dev-wolf.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
+# Installs the dev dependencies for Spider Wolf.
+
 # Exit on any error
 set -e
 
 # Error on undefined variable
 set -u
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+"$script_dir"/install-dev-common.sh
 
 echo "Checking for elevated privileges..."
 privileged_command_prefix=""
@@ -12,14 +17,12 @@ if [ ${EUID:-$(id -u)} -ne 0 ] ; then
   sudo echo "Script can elevate privileges."
   privileged_command_prefix="${privileged_command_prefix} sudo"
 fi
-${privileged_command_prefix} apt-get update
-DEBIAN_FRONTEND=noninteractive ${privileged_command_prefix} apt-get install --no-install-recommends -y \
-    ca-certificates \
+
+DEBIAN_FRONTEND=noninteractive ${privileged_command_prefix} \
+apt-get install --no-install-recommends -y \
     checkinstall \
-    curl \
     g++ \
     gcc \
-    git \
     jq \
     libcurl4 \
     libcurl4-openssl-dev \
@@ -27,16 +30,9 @@ DEBIAN_FRONTEND=noninteractive ${privileged_command_prefix} apt-get install --no
     libssl-dev \
     make \
     openjdk-11-jdk \
-    pkg-config \
-    python3 \
-    python3-pip \
-    python3-venv
+    pkg-config
 
-script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 lib_install_scripts_dir="$script_dir/.."
 ${privileged_command_prefix} "$lib_install_scripts_dir"/install-cmake.sh 3.23.5
 # TODO https://github.com/y-scope/spider/issues/86
 "$lib_install_scripts_dir"/check-cmake-version.sh
-
-# Install uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
