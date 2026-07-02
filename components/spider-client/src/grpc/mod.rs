4ace626699bba869a7d5581dc2e693a1c21598f2
sitaowang1998 <sitaowang1998@outlook.com>
2026-07-02 15:10:09 -0400

    feat(spider-client): Add user-facing client library for job orchestration and resource group management. (#363)
    Co-authored-by: LinZhihao-723 <pleiades3190@gmail.com>

---
+++ components/spider-client/src/grpc/mod.rs
@@ -0,0 +1,4 @@
+//! gRPC clients implementation for Spider services.
+
+pub mod job;
+pub mod resource_group;
---
+++ components/spider-client/src/lib.rs
@@ -0,0 +1,14 @@
+//! User-facing client library for the Spider services.
+//!
+//! This library provides the Rust API for interacting with the Spider services to:
+//!
+//! * Managing Spider resource groups.
+//! * Orchestrating Spider jobs.
+//!
+//! See [`SpiderClient`] for the main client API.
+
+pub mod client;
+pub mod error;
+pub(crate) mod grpc;
+
+pub use client::SpiderClient;
