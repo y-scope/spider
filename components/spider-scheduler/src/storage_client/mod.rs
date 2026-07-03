ac5e5d6e4bda131b3539bbe45acdbc54a528eded
Sitao Wang <sitaowang1998@outlook.com>
2026-07-03 12:16:22 -0400

    Fix docstring

--- components/spider-scheduler/src/storage_client/mod.rs
+++ components/spider-scheduler/src/storage_client/mod.rs
@@ -146,7 +146,7 @@ pub trait SchedulerStorageClient: Send + Sync + Clone {
     /// * [`StorageClientError::Transport`] if the storage server returns malformed data.
     async fn job_state(&self, job_id: JobId) -> Result<JobState, StorageClientError>;
 
-    /// Ask storage to resends ready tasks for all jobs in the cache to the ready queue.
+    /// Asks storage to resends ready tasks for all jobs in the cache to the ready queue.
     ///
     /// # Errors
     ///
