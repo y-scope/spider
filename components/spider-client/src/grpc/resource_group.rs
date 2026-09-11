170a2de81631f990cd14cfa5961c83d6a11b6611
LinZhihao-723 <pleiades3190@gmail.com>
2026-09-11 12:27:00 -0400

    Remove unnecessary unit tests.

--- components/spider-client/src/grpc/resource_group.rs
+++ components/spider-client/src/grpc/resource_group.rs
@@ -138,18 +138,3 @@ fn resource_group_status_to_error(status: &Status) -> ClientError {
         _ => ClientError::Server(status.message().to_owned()),
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn status_maps_already_exists_to_resource_group_already_exists() {
-        let status = Status::already_exists("duplicate external resource group id");
-
-        assert!(matches!(
-            resource_group_status_to_error(&status),
-            ClientError::ResourceGroupAlreadyExists
-        ));
-    }
-}
